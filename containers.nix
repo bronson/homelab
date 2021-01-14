0baf81bd3be943f93c63cce9275acaa3520fb707
Aditya Sharma <git@adi.run>
2021-01-13 21:52:55 -0500

    fix ports

--- containers.nix
+++ containers.nix
@@ -7,11 +7,14 @@
       adguard = {
         autoStart = true;
         image = "adguard/adguardhome";
-        extraOptions = ["--dns=1.1.1.1"];
+        extraOptions = [
+          "--dns=1.1.1.1"
+          "--dns=8.8.8.8"
+        ];
         ports = [
           "5353:53/tcp"
+          "5353:53/udp"
           "8080:80/tcp"
-          "3000:3000/tcp"
         ];
         volumes = [
           "/var/lib/adguardhome/conf:/opt/adguardhome/conf"
